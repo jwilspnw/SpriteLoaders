cbcb9f314a9825586157a815c2e351fe731e2e6a
Justin Wilson <justin.m.wilson@hotmail.com>
2019-08-26 21:31:09 -0700

    Initial Pass at Creating Sprite-like Loading Animations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import logo from './logo.svg'
+import BouncyBarsLoader from './BouncyBarsLoader'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <BouncyBarsLoader />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
